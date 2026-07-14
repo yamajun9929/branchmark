a3f041d0b9a1f953760f75cab7bdecc83e5f94fb
yamano jun <yamanojun@team-lab.com>
2026-07-14 23:14:55 +0900

    Fix Homebrew formula checksum

--- Formula/brmk.rb
+++ Formula/brmk.rb
@@ -2,7 +2,7 @@ class Brmk < Formula
   desc "Branchmark, a low-dependency terminal bookmark tree"
   homepage "https://github.com/yamajun9929/branchmark"
   url "https://github.com/yamajun9929/branchmark/archive/refs/tags/v0.1.0.tar.gz"
-  sha256 "REPLACE_WITH_RELEASE_TARBALL_SHA256"
+  sha256 "87004303a3148e72ed6e659c0add6b9a88ced110fa024603bc40b0b82f2396c7"
   license "MIT"
 
   depends_on "go" => :build
